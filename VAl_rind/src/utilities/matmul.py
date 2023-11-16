ef022a3884cc84058184d261573fe7f3b662ebcf
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-11-16 15:07:56 +0530

    Update matmul.py

--- VAl_rind/src/utilities/matmul.py
+++ VAl_rind/src/utilities/matmul.py
@@ -1,10 +1,22 @@
-def matmul(f_row , f_col , s_row , s_col):
-
-    if len(f_col) != len(s_row) : raise ValueError(
-        f'Cannot nultiply array with dimensions ({len(f_row)}x{len(s_row)}) , ({len(s_row)}x{len(s_col)})')
+def matmul(f_row , f_col , s_row , s_col , funcs = None):
     
-    val = sum(f_col * s_row / 2)
+    if len(f_col) != len(s_row) : raise ValueError(f'Cannot multiply array with dimensions {len(f_row)}x{len(f_col)} , {len(s_row)}x{len(s_col)}')
+    if funcs :
+        
+        if len(funcs) < len(f_col) : 
+            
+            warnings.warn(f'{len(funcs)} < {len(f_col)} : Defining the leftover to be Multiplications')
+            
+            funcs += [lambda x , y : x * y] * (len(s_col) - len(funcs))
+        
+        elif len(funcs) > len(f_col) : raise ValueError(f'{len(funcs)} > {len(f_col)} : Length of funcs should be <= Length of Matrix')
+    else : funcs = [lambda x , y : x * y] * len(s_col)
+            
+    val = sum(list(map(
+        lambda func , x , y : func(x , y) , funcs , f_col , s_row
+    )))
+    
+    col = s_col * val / 2
     row = f_row * 2
-    col = s_col * val
-
-    return (row , col)
+    
+    return row , col
